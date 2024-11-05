9aa2979d8610c4639851755995a6af082d4beb28
Yann <yg.gicquel@gmail.com>
2024-11-05 16:26:42 +0100

    Changement sur Legal

--- src/pages/Legal/Legal.jsx
+++ src/pages/Legal/Legal.jsx
@@ -9,7 +9,7 @@ function Legal() {
 
   return (
     <div className="legal">
-          <h2>Mentions légales</h2>
+      <h2>Mentions légales</h2>
       <div className="legal--wrapper">
         <div className="legal__owner">
           <h3>Editeur du site :</h3>
@@ -27,35 +27,33 @@ function Legal() {
         </div>
         <div className="legal__hosting">
           <h3>Hébergement :</h3>
-          <p>
-            <span className="legal--title">Nom de l'hébergeur : </span>
-            <span className="legal--answer"></span>
-          </p>
-          <p>
-            <span className="legal--title">Adresse postalle : </span>
-            <span className="legal--answer"></span>
-          </p>
-          <p>
-            <span className="legal--title">Adresse e-mail : </span>
-            <span className="legal--answer"></span>
-          </p>
-          <p>
-            <span className="legal--title">Numéro de téléphone : </span>
-            <span className="legal--answer"></span>
-          </p>
+
+          <p className="legal--title">Nom de l'hébergeur : </p>
+          <p className="legal--answer">GitHub Pages</p>
+
+          <p className="legal--title">Adresse postalle : </p>
+          <p className="legal--answer">88 Colin P. Kelly Jr. Street, United States</p>
+
+
+          <p className="legal--title">Les contacter : </p>
+          <p className="legal--answer"><a target="_blank" rel="noreferrer" href="https://docs.github.com/fr/support">https://docs.github.com/fr/support</a></p>
+
+
         </div>
       </div>
-      <h3>Politique de confidentialité</h3>
-      <p>
-        Le contenu de ce site, incluant textes, images et logos, est la
-        propriété exclusive de Yann GICQUEL.
-      </p>
+      <div className="legal__info">
+        <h3>Politique de confidentialité</h3>
+        <p>
+          Le contenu de ce site, incluant textes, images et logos, est la
+          propriété exclusive de Yann GICQUEL.
+        </p>
 
-      <h3>Politique de confidentialité</h3>
-      <p>
-        Les informations collectées via ce site ne sont utilisées que dans le
-        cadre d’une prise de contact et ne sont pas transmises à des tiers.
-      </p>
+        <h3>Politique de confidentialité</h3>
+        <p>
+          Les informations collectées via ce site ne sont utilisées que dans le
+          cadre d’une prise de contact et ne sont pas transmises à des tiers.
+        </p>
+      </div>
     </div>
   );
 }
